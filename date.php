e608dc38ac211cdd9682b86a7edc0081efb234ac
unknown <muzakisyahrul100@gmail.com>
2021-03-23 20:37:37 +0700

    Buat task 1 sampai 6

--- date.php
+++ date.php
@@ -1,4 +1,5 @@
 <?php
+// Task 1
 echo date("l");
 echo "<br/>";
 echo date("d");
@@ -8,3 +9,37 @@ echo "<br/>";
 echo date("m");
 echo "<br/>";
 echo date("Y");
+echo "<br/>";
+echo time();
+echo "<br/>";
+
+// Task 2
+echo date(
+    "l, d-M-Y",
+    time() +
+        60 * 60 * 24 * 100 // 100 hari mendatang
+);
+echo "<br/>";
+
+// Task 3
+echo date(
+    "l, d-M-Y",
+    time() +
+        60 * 60  * 24 * 1000 // 1000 hari mendatang
+);
+echo "<br/>";
+
+// Task 4
+echo date(
+    "l, d-M-Y",
+    time() -
+        60 * 60  * 24 * 500 // 500 hari lampau
+);
+echo "<br/>";
+
+// Task 5 
+echo date("l", mktime(0, 0, 0, 12, 21, 2021)); // hari lahir saya
+echo "<br/>";
+
+// Task 6 
+echo date("l", strtotime("21 December 2030")); // hari ulang tahun saya di tahun 2030
